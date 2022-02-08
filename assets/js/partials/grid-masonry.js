23a4dc2ce8d91692db3b44fe9eec77a21e21cf82
uhahaha2000 <uhahaha2000@naver.com>
2022-02-08 17:40:27 +0900

    posts list - grid-masonry 적용

--- assets/js/partials/grid-masonry.js
+++ assets/js/partials/grid-masonry.js
@@ -8,10 +8,8 @@ function masonryLayout() {
 	const autoRows = parseInt(
 		masonryContainerStyle.getPropertyValue("grid-auto-rows")
 	);
+
 	document.querySelectorAll(".post-teaser").forEach((elt) => {
-		elt.style.height =  Math.ceil(Math.random()*(300-30)+50) + 'px';
-	});
-	document.querySelectorAll(".masonry-item").forEach((elt) => {
 		elt.style.gridRowEnd = `span ${Math.ceil(
 			elt.querySelector(".post-teaser").scrollHeight / autoRows + columnGap / autoRows
 		)}`;
